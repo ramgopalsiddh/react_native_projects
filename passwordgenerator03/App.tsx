9450e3a9318376baaf5c258ee7a3d72bd07723d2
ramgopalsiddh <ramgopalsiddh.000@gmail.com>
2024-10-13 19:10:32 +0530

    Added useState in app

--- passwordgenerator03/App.tsx
+++ passwordgenerator03/App.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 // Form validation
 import * as Yup from 'yup'
@@ -12,6 +12,24 @@ const PasswordSchema = Yup.object().shape({
 })
 
 export default function App() {
+
+  const [password, setPassword] = useState('')
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+
+  const [lowerCase, setLowerCase] = useState(false)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, useNumbers] = useState(false)
+  const [symbols, usesymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength: number) => {
+    //
+  }
+
+  const resetPasswordState = () => {
+    //
+  }
+
+
   return (
     <View>
       <Text>App</Text>
