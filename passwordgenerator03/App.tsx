ef4ef69f082a6bdb84c5ca729887a6624985c344
ramgopalsiddh <ramgopalsiddh.000@gmail.com>
2024-10-13 18:09:35 +0530

    Created new project Password generator

---
+++ passwordgenerator03/App.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+// Form validation
+import * as Yup from 'yup'
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+  .min(4, 'should be min of 4 characters')
+  .max(16, 'should be max of 16 characters')
+  .required('Length is required')
+})
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
