b698c682e1b03195984adb0c1027fdb4b5c58bfd
ramgopalsiddh <ramgopalsiddh.000@gmail.com>
2024-10-20 18:51:38 +0530

    Add nevigation in details page

--- nevigationexplore09/src/screens/Details.tsx
+++ nevigationexplore09/src/screens/Details.tsx
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Details = () => {
+// Navigation
+import {NativeStackScreenProps} from "@react-navigation/native-stack"
+import {RootStackParamList} from '../App'
+import { useNavigation } from '@react-navigation/native'
+import {NativeStackNavigationProp} from '@react-navigation/native-stack'
+
+
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>
+
+
+const Details = ({route}: DetailsProps) => {
+
+    const {productId} = route.params
+
+    const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={styles.container}>
+      <Text style={styles.smallText}>Details: {productId}</Text>
+      <Button 
+      title='Go to Home'
+      // onPress={() => navigation.navigate("Home")}
+      onPress={() => navigation.goBack()}
+      />
+      <Button 
+      title='Go back to first screen'
+      // onPress={() => navigation.pop(2)}
+      onPress={() => navigation.popToTop()}
+
+      />
     </View>
   )
 }
